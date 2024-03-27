561d920b0dc09fbf65950035e648d4508e724a46
CJr <867525213@qq.com>
2024-03-27 13:45:29 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import Header from './components/Layout/Header.vue';
+
+const HeaderRoutes = [
+  { path: '/', name: 'Home' },
+  { path: '/about', name: '会话' },
+  { path: '/Home', name: '历史消息' },
+  { path: '/Login', name: '群发助手'},
+  { path: '/Login', name: '朋友圈'},
+  { path: '/Login', name: '客户中心'},
+  { path: '/Login', name: '报表统计'},
+  { path: '/Login', name: '设计'}
+]
+
+</script>
+
+<template>
+  <Header :routes="HeaderRoutes" />
+  <RouterView />
+</template>
+
+<style>
+</style>
---
+++ src/components/Layout/Header.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="header">
+    <div class="header__nav">
+      <router-link
+        v-for="route in props.routes"
+        :key="route.path"
+        :to="route.path"
+        class="nav__item"
+        :class="{ 'nav__item--active': $route.path === route.path }"
+      >
+        {{ route.name }}
+      </router-link>
+    </div>
+  </div>
+</template>
+  
+<script setup lang='ts'>
+import { defineProps,ref } from 'vue'
+interface Route {
+  path: string;
+  name: string;
+}
+const props = defineProps({
+  routes: Array<Route>
+})
+
+</script>
+  
+<style lang="scss" scoped>
+
+$themColor: green;
+  
+  .header {
+    width: 100%;
+    height: 50px;
+    background-color: #333;
+
+    &__nav {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100%;
+      color: #fff;
+
+      .nav__item {
+        height: 100%;
+        margin: 0 10px;
+        color: #fff;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-decoration: none;
+        
+        &--active {
+          color: $themColor;
+          border-bottom: 3px solid $themColor;
+        }
+      }
+    }
+  }
+
+</style>
